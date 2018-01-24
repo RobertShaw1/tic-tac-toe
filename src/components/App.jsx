e90591f3d601069fb980bc425645467b21bf40a1
Brett F <brettfox11@gmail.com>
2018-01-24 17:22:51 -0600

    Beginning UI skeleton

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,36 @@
 //below allows for using Component instead of react.Component
-import React, {Component} from "react";
+import React, { Component } from "react";
 
-function App(){
-    return (
-        <h1>
-            Hello, world!
-        </h1>
-    )
-} 
+function App() {
+  return (
+    <div>
+      <h1>
+        Hello, react--from Wombat!
+      </h1>
+      <Table />
+    </div>
+  )
+}
+
+function Row(props){
+  return (
+    <tr>
+      {props.cellNumbers.map(cell =>
+      <td> {cell} </td>
+      )}
+    </tr>
+  )
+}
+
+
+function Table() {
+  return (
+    <table>
+      <Row cellNumbers = {[0,1,2]}/>
+      <Row cellNumbers = {[3,4,5]}/>
+      <Row cellNumbers = {[6,7,8]}/>
+    </table>
+  )
+}
 
 export default App;
